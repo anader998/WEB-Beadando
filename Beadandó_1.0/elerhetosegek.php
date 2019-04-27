57a353157437d4f177eea203d3151ac6a5f3913d
anader998 <49987403+anader998@users.noreply.github.com>
2019-04-27 18:49:06 +0200

    Add files via upload

---
+++ Beadandó_1.0/elerhetosegek.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+   
+    
+    <link rel="stylesheet" type="text/css" href="beadando1.css">
+    
+</head>
+<body>
+    
+    
+    
+    <header>
+        <a href="Főoldal_1"><img src="img/logo.png"></a>
+        <title>Elérhetőségek</title>
+        
+    
+    </header>
+    <div id="menu">
+                <ul>
+                     <li><a href="kezdolap.php"><strong>Főoldal</strong></a></li>
+                    <li><a href="informacio.php"><strong>Információ</strong></a></li>
+                    <li><a href="projectek.php"><strong>Projektek</strong></a></li>
+                    <li><a href="kozerdeku.php"><strong>Küzérdekű adatok</strong></a></li>
+                    <li><a href="elerhetosegek.php"><strong>Elérhetőségek</strong></a></li>
+					<li><a href="galeria.php"><strong>Galéria</strong></a></li>
+					<li><a href="kapcsolat.php"><strong>Kapcsolat</strong></a></li>
+					<li><a href="video.php"><strong>Videók</strong></a></li>
+					<li><a href="belep.php"><strong>Belépés/Regisztráció</strong></a></li>
+                    
+                </ul>
+    </div>
+        <div id="google">
+        <form method="get" action="http://www.google.com/search">
+            <input type="text"   name="q" size="15"axlength="255" value="Keresés az oldalon" />
+            <input type="submit" value="Google keresés" />
+        </form>
+		<tr>
+                <td >
+                   
+                          Az eredeti weboldal: <a href="http://neum.hu/welcome.html">Link</a>
+                    
+                </td>
+            </tr>
+    <div id="felirat">
+        <a id="eler"><img src="eler.png"width="100%"></a>
+        <h2>Elérhetőségek:</h2>
+		
+        <p>
+		Tel: +363344555
+		</p>
+       <p>
+	   Elmail: info@neuma.hu
+	   </p>
+       
+       
+</body>
+</html>
+
---
+++ Beadandó_1.0/informacio.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+   
+     <title>Információ</title>
+    <link rel="stylesheet" type="text/css" href="beadando1.css">
+    
+</head>
+<body>
+    
+    
+    
+    <header>
+        <a href="Főoldal_1"><img src="img/logo.png"></a>
+       
+        
+    
+    </header>
+    <div id="menu">
+                <ul>
+                     <li><a href="kezdolap.php"><strong>Főoldal</strong></a></li>
+                    <li><a href="informacio.php"><strong>Információ</strong></a></li>
+                    <li><a href="projectek.php"><strong>Projektek</strong></a></li>
+                    <li><a href="kozerdeku.php"><strong>Küzérdekű adatok</strong></a></li>
+                    <li><a href="elerhetosegek.php"><strong>Elérhetőségek</strong></a></li>
+					<li><a href="galeria.php"><strong>Galéria</strong></a></li>
+					<li><a href="kapcsolat.php"><strong>Kapcsolat</strong></a></li>
+					<li><a href="video.php"><strong>Videók</strong></a></li>
+					<li><a href="belep.php"><strong>Belépés/Regisztráció</strong></a></li>
+                 
+                </ul>
+    </div>
+        <div id="google">
+        <form method="get" action="http://www.google.com/search">
+            <input type="text"   name="q" size="15"axlength="255" value="Keresés az oldalon" />
+            <input type="submit" value="Google keresés" />
+        </form>
+		</div>
+		<tr>
+                <td >
+                   
+                          Az eredeti weboldal: <a href="http://neum.hu/welcome.html">Link</a>
+                    
+                </td>
+            </tr>
+    <div id="felirat">
+        <p>
+		
+        <h1>Információ</h1>
+		<h4>Korményrendeleti kivonat</h4>
+		<h4>Tevékenységeink</h4>
+		127/2017. (VI. 8.) Kormányrendelet a Digitális Jólét Program végrehajtásával összefüggő egyes feladatokról, valamint a Kormányzati Informatikai Fejlesztési Ügynökségről szóló 268/2010. (XII. 3.) Kormányrendelet módosítása alapján a Neumann Nonprofit Közhasznú Kft. ellátja az információs társadalom fejlesztésével, a digitális készségek fejlesztésével, és az e területen működő programok koordinációjával kapcsolatos feladatokat az iskolarendszeren kívüli képzések vonatkozásában, valamint a DJP hálózat szakmai koordinálásával kapcsolatos feladatokat, a DJP eredményeinek országos szintű megjelenítését, az érintett szereplők tájékoztatását, felügyeli és koordinálja a Digitális Jólét Program Mentorok munkáját. Képzési, valamint az információs társadalom fejlesztésével és a DJP Hálózat szakmai koordinálásával kapcsolatos felnőttképzési tevékenységet végez. Részt vesz (GINOP 3.3.2.) az „Online kormányzati, közigazgatási és e-egészségügyi szolgáltatások terjedésének és a Digitális Jólét Program kiterjesztésének elősegítése” című kiemelt projekt végrehajtásában. Részt vesz az „Oktatási intézmények és IKT vállalkozások közötti együttműködés ösztönzése és támogatása” című kiemelt projekt végrehajtásában, működteti (GINOP 3.1.3.) a kezdő informatikai vállalkozások nemzetközi piacra lépését, valamint részt vesz a startup ökoszisztéma fejlesztését szolgáló kormányzati kezdeményezések végrehajtásában.
+
+
+		
+		</p>
+        
+    </div>
+       
+       
+       
+</body>
+</html>
+
